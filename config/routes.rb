ef3554a19ee0d2c6121e1357d013378a265fdc79
Lee Adkins <lee@ravsonic.com>
2009-12-27 08:44:02 -0600

    more admin area cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :wanteds, :as => "most-wanted"
   map.resources :posts
   map.resources :pages
-  map.resources :users
+  #Uncomment below to allow users to be created from the main site 
+  #map.resources :users
   map.resources :user_sessions
   # The priority is based upon order of creation: first created -> highest priority.
 
