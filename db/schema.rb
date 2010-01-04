89e63118d0ab1646f426bf700f3625f60fb54391
Lee Adkins <lee@ravsonic.com>
2010-01-04 13:15:37 -0600

    Beginning work on a tree layout

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100104183135) do
+ActiveRecord::Schema.define(:version => 20100104190558) do
 
   create_table "assets", :force => true do |t|
     t.string   "name"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(:version => 20100104183135) do
     t.integer  "rank"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "parent"
   end
 
   create_table "posts", :force => true do |t|
