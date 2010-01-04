16326a9bd5f29fc5a2ef9bb3dcc87186e01231ad
Lee Adkins <lee@ravsonic.com>
2010-01-04 12:36:13 -0600

    Adding Versioning to Pages

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,5 @@
 class Page < ActiveRecord::Base
+  versioned
 validates_uniqueness_of :slug, :on => :create, :message => "must be unique"
 validates_presence_of :title, :on => :create, :message => "can't be blank"
 validates_uniqueness_of :menu, :on => :create, :message => "must be unique"
