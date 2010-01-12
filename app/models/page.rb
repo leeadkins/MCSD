d65afc62d548e996343677a881c5d26a59ef1fed
Lee Adkins <lee@ravsonic.com>
2010-01-12 12:57:05 -0600

    Cleaning up the model a bit

--- app/models/page.rb
+++ app/models/page.rb
@@ -6,7 +6,6 @@ validates_uniqueness_of :menu, :on => :create, :message => "must be unique"
 validates_presence_of :menu, :on => :create, :message => "can't be blank"
 validates_presence_of :slug, :on => :create, :message => "can't be blank"
 
-
-  named_scope :published, :conditions => {:published => true}
+named_scope :published, :conditions => {:published => true}
 
 end
--- app/models/wanted.rb
+++ app/models/wanted.rb
@@ -5,4 +5,6 @@ class Wanted < ActiveRecord::Base
                                 :thumb => "100x100>"}
                                 
   validates_attachment_content_type :mug, :content_type => ['image/jpeg', 'image/png']
+  validates_presence_of :lastname, :on => :create, :message => "can't be blank"
+  validates_presence_of :firstname, :on => :create, :message => "can't be blank"
 end
